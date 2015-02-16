ea9cf9a4e2e9918dfc6af039088cfccdb67837ec
SangChan Lee <sangchan82@gmail.com>
2015-02-16 10:51:28 +0900

    Initial Commit

---
+++ TestCoreData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TestCoreData
+//
+//  Created by SangChan on 2015. 2. 16..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TestCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCoreData
+//
+//  Created by SangChan on 2015. 2. 16..
+//  Copyright (c) 2015년 sangchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
